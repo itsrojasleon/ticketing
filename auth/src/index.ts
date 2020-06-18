2bc50520e0e9d6a70213c5c1c76b928441c442fc
Juan Luis Rojas León <rojasleon122@gmail.com>
2020-06-18 17:19:00 -0500

    remove console.log

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('Starting up server');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
